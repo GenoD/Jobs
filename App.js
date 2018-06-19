f624faf9f28efd205684f8cf840d92cb1d73df86
GenoDiaz <genodiaz@yahoo.com>
2018-06-18 23:31:08 -0700

    Initialize

---
+++ App.js
@@ -0,0 +1,30 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+export default class App extends React.Component {
+  render() {
+    const MainNavigator = createBottomTabNavigator({
+      //CHANGE!!!
+      welcome: WelcomeScreen,
+      auth: AuthScreen
+    });
+    console.log('Main Navigator', MainNavigator);
+    return (
+      <View style={styles.container}>
+        <MainNavigator />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+});
