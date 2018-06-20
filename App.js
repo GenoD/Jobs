7595074ee80bfea44598c5d037d7f449166ec57d
GenoDiaz <genodiaz@yahoo.com>
2018-06-19 23:10:44 -0700

    Basic navigation and tree setup

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
   render() {
     const MainNavigator = createBottomTabNavigator({
       //CHANGE!!!
       welcome: WelcomeScreen,
-      auth: AuthScreen
+      auth: AuthScreen,
+      main: {
+        screen: createBottomTabNavigator({
+            map: MapScreen,
+            deck: DeckScreen,
+            review: createStackNavigator({
+              review: ReviewScreen,
+              settings: SettingsScreen,
+            })
+        })
+      }
     });
     console.log('Main Navigator', MainNavigator);
     return (
